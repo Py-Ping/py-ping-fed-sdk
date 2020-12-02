2537c5644d1bfda7d8cffcf44873bfe161dc3854
ted.devaal <ted.devaal@versent.com.au>
2020-12-02 12:26:21 +1100

    Test docker generate

--- src/docker_generate.py
+++ src/docker_generate.py
@@ -126,10 +126,8 @@ if __name__ == "__main__":
     add_args()
     args = parser.parse_args()
     home = os.environ["HOME"]
-    ping_user = os.environ["PING_IDENTITY_DEVOPS_USER"].split('"')[3]
-    ping_key = os.environ["PING_IDENTITY_DEVOPS_KEY"].split('"')[3]
-    print(ping_user)
-    print(ping_key)
+    ping_user = os.environ["PING_IDENTITY_DEVOPS_USER"]
+    ping_key = os.environ["PING_IDENTITY_DEVOPS_KEY"]
     endpoint = "https://localhost:9999/pf-admin-api/v1"
     swagger_url = f"{endpoint}/api-docs"
     session = get_auth_session()
